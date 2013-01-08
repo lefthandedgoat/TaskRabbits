79c6b4f3127307c2182414f2d2718560401525bc
Amir Rajan <ar@amirrajan.net>
2013-01-08 17:58:24 -0600

    bare bones ready for hacking

---
+++ TaskRabbits/Controllers/RabbitsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace TaskRabbits.Controllers
+{
+    public class RabbitsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return null;
+        }
+    }
+}
---
+++ TaskRabbits/Controllers/TasksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace TaskRabbits.Controllers
+{
+    public class TasksController : Controller
+    {
+        public ActionResult Index(int rabbitId)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Update()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            return null;
+        }
+    }
+}
