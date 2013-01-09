db5bc87b06da130692b8780c52813ac82df1675a
Amir Rajan <ar@amirrajan.net>
2013-01-08 18:37:12 -0600

    all stubbed out

--- TaskRabbits/Controllers/RabbitsController.cs
+++ TaskRabbits/Controllers/RabbitsController.cs
@@ -4,54 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Oak;
-using TaskRabbits.Repositories;
-using TaskRabbits.Models;
 
 namespace TaskRabbits.Controllers
 {
     public class RabbitsController : Controller
     {
-        Rabbits rabbits = new Rabbits();
-
         [HttpGet]
         public ActionResult Index()
         {
-            var result = rabbits.All();
-
-            result.ForEach(AddLinks);
-
-            return new DynamicJsonResult(new
+            var rabbits = new[] 
             {
-                Rabbits = result,
+                new { Id = 1, Name = "Test", TasksUrl = "/Tasks?rabbitId=1" },
+                new { Id = 2, Name = "Test 2", TasksUrl = "/Tasks?rabbitId=2" }
+            };
+
+            return Json(new
+            {
+                Rabbits = rabbits,
                 CreateRabbitUrl = Url.RouteUrl(new { controller = "Rabbits", action = "Create" })
-            });
+            }, 
+            JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
-        public ActionResult Create(dynamic @params)
+        public ActionResult Create()
         {
-            dynamic rabbit = new Rabbit(@params);
-
-            if(rabbit.IsValid())
+            if (true /*isvalid*/)
             {
-                rabbit.Save();
-                AddLinks(rabbit);
-                return new DynamicJsonResult(rabbit);
+                return Json(new
+                {
+                    Id = 3,
+                    Name = "Test 3",
+                    TasksUrl = "/Tasks?rabbitId=3"
+                },
+                JsonRequestBehavior.AllowGet);
             }
             else
             {
-                return new DynamicJsonResult(new { Errors = rabbit.Errors() });
-            }
-        }
+                var errorPayload = new
+                {
+                    Errors = new[] 
+                    {
+                        new { Key = "Name", Value = "Name is required." },
+                        new { Key = "Name", Value = "Name must be unique." }
+                    }
+                };
 
-        public void AddLinks(dynamic rabbit)
-        {
-            rabbit.TasksUrl = Url.RouteUrl(new
-            {
-                controller = "Tasks",
-                action = "Index",
-                rabbitId = rabbit.Id
-            });
+                return Json(errorPayload, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
--- TaskRabbits/Controllers/TasksController.cs
+++ TaskRabbits/Controllers/TasksController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TaskRabbits.Repositories;
 using Oak;
 using System.Web.Routing;
 
