771c9bfe4c94f00d164f685de3c0e1e5da813779
Amir Rajan <ar@amirrajan.net>
2013-01-09 00:11:32 -0600

    amir's branch

---
+++ TaskRabbits/Repositories/Rabbits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Massive;
+using Oak;
+
+namespace TaskRabbits.Repositories
+{
+    public class Rabbits : DynamicRepository
+    {
+        //static Rabbits()
+        //{
+        //    Gemini.Extend<Rabbits, Memoize>();
+        //}
+
+        //IEnumerable<dynamic> Memoize()
+        //{
+        //    yield return (DynamicFunctionWithParam)CoolRabbits;
+        //}
+
+        //dynamic CoolRabbits(dynamic args)
+        //{
+        //    //long running query
+        //    return null;
+        //}
+    }
+}
